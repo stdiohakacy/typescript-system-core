911bd18628d9e863cc74d070dad4f4b39765e5d7
Duy Nguyen <duynguyen@Duys-MacBook-Pro-2.local>
2023-06-29 13:18:58 +0700

    set up swagger and refactor main nest app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,91 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestApplication, NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import * as compression from 'compression';
+import * as morgan from 'morgan';
+import { ConfigService } from '@nestjs/config';
+import { Logger, VersioningType } from '@nestjs/common';
+import { useContainer } from 'class-validator';
+import { middleware as expressCtx } from 'express-ctx';
+import setupSwagger from './swagger';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+export async function bootstrap() {
+  const app = await NestFactory.create<NestApplication>(
+    AppModule,
+    new ExpressAdapter(),
+    {
+      cors: true,
+    },
+  );
+
+  const configService = app.get(ConfigService);
+  const databaseUri: string = configService.get<string>('database.host');
+  const env: string = configService.get<string>('app.env');
+  const port: number = configService.get<number>('app.http.port');
+  const globalPrefix: string = configService.get<string>('app.globalPrefix');
+  const versioningPrefix: string = configService.get<string>(
+    'app.versioning.prefix',
+  );
+  const version: string = configService.get<string>('app.versioning.version');
+
+  // enable
+  const httpEnable: boolean = configService.get<boolean>('app.http.enable');
+  const versionEnable: string = configService.get<string>(
+    'app.versioning.enable',
+  );
+  const jobEnable: boolean = configService.get<boolean>('app.jobEnable');
+  const documentationEnable: boolean = configService.get<boolean>(
+    'app.documentation.enable',
+  );
+
+  const logger = new Logger();
+  process.env.NODE_ENV = env;
+
+  app.setGlobalPrefix(globalPrefix);
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
+
+  app.use(compression());
+  app.use(morgan('combined'));
+
+  // Versioning
+  if (versionEnable) {
+    app.enableVersioning({
+      type: VersioningType.URI,
+      defaultVersion: version,
+      prefix: versioningPrefix,
+    });
+  }
+
+  if (documentationEnable) {
+    setupSwagger(app);
+  }
+
+  app.use(expressCtx);
+  await app.listen(port);
+  logger.log(`==========================================================`);
+  logger.log(`Environment Variable`, 'NestApplication');
+  logger.log(JSON.parse(JSON.stringify(process.env)), 'NestApplication');
+  logger.log(`==========================================================`);
+  logger.log(`Job is ${jobEnable}`, 'NestApplication');
+  logger.log(
+    `Http is ${httpEnable}, ${
+      httpEnable ? 'routes registered' : 'no routes registered'
+    }`,
+    'NestApplication',
+  );
+  logger.log(`Http versioning is ${versionEnable}`, 'NestApplication');
+  logger.log(`Http Server running on ${await app.getUrl()}`, 'NestApplication');
+  logger.log(`Database uri ${databaseUri}`, 'NestApplication');
+  logger.log(`==========================================================`);
+  return app;
 }
-bootstrap();
+void bootstrap();
---
+++ src/swagger.ts
@@ -0,0 +1,58 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestApplication } from '@nestjs/core';
+import { ENUM_APP_ENVIRONMENT } from './app/constants/app.enum.constant';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ResponseDefaultSerialization } from './common/response/serialization/repsonse.default.serialization';
+
+export default async function (app: NestApplication) {
+  const configService = app.get(ConfigService);
+  const env: string = configService.get<string>('app.env');
+  const logger = new Logger();
+
+  const docName: string = configService.get<string>('doc.name');
+  const docDesc: string = configService.get<string>('doc.description');
+  const docVersion: string = configService.get<string>('doc.version');
+  const docPrefix: string = configService.get<string>('doc.prefix');
+
+  if (env !== ENUM_APP_ENVIRONMENT.PRODUCTION) {
+    const documentBuild = new DocumentBuilder()
+      .setTitle(docName)
+      .setDescription(docDesc)
+      .setVersion(docVersion)
+      .addTag("API's")
+      .addServer(`/`)
+      .addServer(`/staging`)
+      .addBearerAuth(
+        { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+        'accessToken',
+      )
+      .addBearerAuth(
+        { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+        'refreshToken',
+      )
+      .addApiKey({ type: 'apiKey', in: 'header', name: 'x-api-key' }, 'apiKey')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, documentBuild, {
+      deepScanRoutes: true,
+      extraModels: [
+        ResponseDefaultSerialization,
+        // ResponsePagingSerialization,
+        // AwsS3MultipartPartsSerialization,
+        // AwsS3MultipartSerialization,
+        // AwsS3Serialization,
+      ],
+    });
+
+    SwaggerModule.setup(docPrefix, app, document, {
+      explorer: true,
+      customSiteTitle: docName,
+      swaggerOptions: { persistAuthorization: true },
+    });
+
+    logger.log(`==========================================================`);
+    logger.log(`Docs will serve on ${docPrefix}`, 'NestApplication');
+    logger.log(`==========================================================`);
+  }
+}
